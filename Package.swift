8a0419f36fd69022ce9ec9e1528c009f2e5b13e0
Eric Rabil <ericjrabil@gmail.com>
2021-10-25 20:15:09 -0400

    bump platform requirements

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BarcelonaFoundation",
     platforms: [
-        .macOS(.v10_14), .iOS(.v13)
+        .macOS(.v10_15), .iOS(.v13)
     ],
     products: [
         .library(
